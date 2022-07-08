b9b76012fc37e08cb56b2840a72f4a59d7c7a28c
Owoeye precious <72979479+parallelbox-lab@users.noreply.github.com>
2022-07-08 23:40:45 +0100

    commit

---
+++ lib/screens/articles/articles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Articles extends StatelessWidget {
+  const Articles({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
