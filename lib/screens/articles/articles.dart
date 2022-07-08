9301f9cc0128300547bf76a1bb590fa8b870c6e4
Owoeye precious <72979479+parallelbox-lab@users.noreply.github.com>
2022-07-08 23:45:57 +0100

    commit

--- lib/screens/articles/articles.dart
+++ lib/screens/articles/articles.dart
@@ -8,7 +8,13 @@ class Articles extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body:ListView(
-          
+          children: [
+            Row(children: [
+              IconButton(onPressed: 
+              (){}, 
+              icon:const Icon(Icons.arrow_back_ios))
+            ],)
+          ],
         )
       ),
     );
